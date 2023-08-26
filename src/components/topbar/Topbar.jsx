0187e28c3c5f11fa7f3100e1d6e909f291323518
Roshini Dias <jbrnd21@gmail.com>
2023-08-27 01:56:25 +0530

    set some basic components and pages

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import './topbar.css'
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">topbar left</div>
+      <div className="topbarCenter">topbar center</div>
+      <div className="topbarRight">topbar right</div>
+    </div>
+  )
+}
