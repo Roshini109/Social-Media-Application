c0801959c924aa81d8117b8547c7c2e76a68a8b8
Roshini Dias <jbrnd21@gmail.com>
2023-08-27 18:00:36 +0530

    Create Righbar Section

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,24 @@ import './rightbar.css'
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/icons/bd2.png" alt="" />
+          <span className="birthdayText"><b>Pola Foster</b> and <b>3 others </b> 
+          have birthdays today! </span>
+        </div>
+        <img src="/assets/adv/a4.jpg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/p5.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUseraName">John Carter</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
