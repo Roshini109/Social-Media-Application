8de2754dde0aca24e2ef72d1aee5d67846c4a9d5
Roshini Dias <jbrnd21@gmail.com>
2023-08-27 20:05:26 +0530

    Test with dummy data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,33 +1,36 @@
 import './post.css'
 import {MoreVert} from '@mui/icons-material'
+import { Users } from '../../dummyData'
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="/assets/person/p3.jpeg" alt="" className="postProfileImg" />
-                    <span className="postUserName">Safak Kogalu</span>
-                    <span className="postDate">5 min ago</span>
+                    <img src={Users.filter((u)=>u.id===post?.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                    <span className="postUserName">
+                        {Users.filter((u)=>u.id===post?.userId)[0].username}
+                        </span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert/>
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey It is my first Post</span>
-                <img src="/assets/posts/po1.jpg" alt="" className="postImg" />
+                <span className="postText">{post?.desc}</span>
+                <img src={post.photo} alt="" className="postImg" />
                 </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img className="likeIcon" src="/assets/icons/pn2.png" alt="" />
                     <img className="likeIcon" src="/assets/icons/pn4.png" alt="" />
                     <img className="likeIcon" src="/assets/icons/pn6.png" alt="" />
-                    <span className="postLikeCounter">32 people liked it</span>
+                    <span className="postLikeCounter">{post.like} people liked it</span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText">9 comments</span>
+                    <span className="postCommentText">{post.comment} comments</span>
                 </div>
             </div>
         </div>
---
+++ src/dummyData.js
@@ -0,0 +1,109 @@
+export const Users =[
+    {
+        id:1,
+        profilePicture:"assets/person/p1.jpg",
+        username:"Safak Kocagalo",
+    },
+    {
+        id:2,
+        profilePicture:"assets/person/p2.jpg",
+        username:"Shehan Vanhoff",
+    },
+    {
+        id:3,
+        profilePicture:"assets/person/p3.jpeg",
+        username:"Sarah Sanjalee",
+    },
+
+    {
+        id:4,
+        profilePicture:"assets/person/p4.jpg",
+        username:"Geremy Lidzan",
+    },
+    {
+        id:5,
+        profilePicture:"assets/person/p5.jpg",
+        username:"Jenny Abreham",
+    },
+    {
+        id:6,
+        profilePicture:"assets/person/p6.jpg",
+        username:"Safak James",
+    },
+    {
+        id:7,
+        profilePicture:"assets/person/p7.jpg",
+        username:"Shalu Tizyan",
+    },
+    {
+        id:8,
+        profilePicture:"assets/person/p8.jpeg",
+        username:"Kamran Kalo",
+    },
+    {
+        id:9,
+        profilePicture:"assets/person/p9.jpeg",
+        username:"Jyashi Shalo",
+    },
+    {
+        id:10,
+        profilePicture:"assets/person/p10.jpg",
+        username:"Hana Shafa",
+    },
+];
+
+export const Posts =[
+    {
+        id:1,
+        desc:"Evenings",
+        photo: "/assets/posts/po1.jpg",
+        date:"5 min ago",
+        userId:1,
+        like:32,
+        comment:9,
+    },
+    {
+        id:2,
+        desc:"Take a break and refresh yourself!",
+        photo: "/assets/posts/po2.jpg",
+        date:"10 min ago",
+        userId:2,
+        like:56,
+        comment:10,
+    },
+    {
+        id:3,
+        desc:"Travelling with lovely ones are the best",
+        photo: "/assets/posts/po3.jpeg",
+        date:"1 hour ago",
+        userId:3,
+        like:1,
+        comment:2,
+    },
+    {
+        id:4,
+        photo: "/assets/posts/po4.jpeg",
+        date:"45 min ago",
+        userId:4,
+        like:12,
+        comment:1,
+    },
+    {
+        id:5,
+        desc:"Love for all, Hatred for none!",
+        photo: "/assets/posts/po5.jpeg",
+        date:"5 min ago",
+        userId:5,
+        like:3,
+        comment:0,
+    },
+    {
+        id:6,
+        desc:"Love for all, Hatred for none!",
+        photo: "/assets/posts/po6.jpg",
+        date:"2 hours ago",
+        userId:6,
+        like:105,
+        comment:10,
+    },
+];
